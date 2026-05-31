108cb9cad7d5fcf63c79d7309e5d83b9c55e4607
ValentinaK83 <kulikova83@yandex.ru>
2026-05-31 19:07:29 +0700

    Update Decepticon.java
    Добавила модификаторы private, getter-ы и setter-ы (сперва внесла изменения в проект в idea, но почему то commit с изменениями не получается сделать в idea, поэтому внесла изменения в сразу здесь)

--- crs/homework21/Decepticon.java
+++ crs/homework21/Decepticon.java
@@ -1,9 +1,33 @@
 package homework21;
 
 public class Decepticon extends Transformer {
-    String teamName;
-    static String eyeColor = "red";
-    boolean kindness = false;
+    private String teamName;
+    private static String eyeColor = "red";
+    private boolean kindness = false;
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public static String getEyeColor() {
+        return eyeColor;
+    }
+
+    public static void setEyeColor(String eyeColor) {
+        Decepticon.eyeColor = eyeColor;
+    }
+
+    public boolean isKindness() {
+        return kindness;
+    }
+
+    public void setKindness(boolean kindness) {
+        this.kindness = kindness;
+    }
 
     Decepticon(String name, String teamName, boolean kindness) {
         super(name);
